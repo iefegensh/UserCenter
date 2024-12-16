0d60210a469b303eff32e9a3ea1feb073711b869
iefegensh <gefeis3@uw.edu>
2024-12-16 12:43:11 -0800

    changes on basic layout (header, content and footer)

---
+++ user-center-frontend-vue/src/components/GlobalHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="globalHeader">
+    <a-menu v-model:selectedKeys="current" mode="horizontal" :items="items" />
+  </div>
+</template>
+<script lang="ts" setup>
+import { h, ref } from "vue";
+import { HomeOutlined, CrownOutlined } from "@ant-design/icons-vue";
+import { MenuProps } from "ant-design-vue";
+
+const current = ref<string[]>(["mail"]);
+const items = ref<MenuProps["items"]>([
+  {
+    key: "/",
+    icon: () => h(HomeOutlined),
+    label: "Home",
+    title: "Home",
+  },
+  {
+    key: "/user/login",
+    label: "User Login",
+    title: "User Login",
+  },
+  {
+    key: "/user/register",
+    label: "User Register",
+    title: "User Register",
+  },
+  {
+    key: "/admin/userMange",
+    icon: () => h(CrownOutlined),
+    label: "User Management",
+    title: "User Management",
+  },
+  {
+    key: "others",
+    label: h(
+      "a",
+      { href: "https://iefegensh.github.io/GefeiShen/", target: "_blank" },
+      "Gefei Shen's Personal Webpage"
+    ),
+    title: "Gefei Shen's Personal Webpage",
+  },
+]);
+</script>

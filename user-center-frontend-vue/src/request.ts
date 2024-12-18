6e21c47012a0eaa6724300e10b12d453a583dea2
iefegensh <gefeis3@uw.edu>
2024-12-18 07:38:20 -0800

    before backend get in

---
+++ user-center-frontend-vue/src/request.ts
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+alert(process.env.NODE_ENV);
+
+const myAxios = axios.create({
+  baseURL: "http://localhost:8080",
+  timeout: 1000,
+  withCredentials: true,
+});
+
+// Add a request interceptor
+myAxios.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+myAxios.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    console.log(response);
+
+    const { data } = response;
+    console.log(data);
+    // if not login
+    if (data.code === 40100) {
+      // If it's not the interface to get user information, or if it's not the login page, then jump to the login page.
+      if (
+        !response.request.responseURL.includes("user/current") &&
+        !window.location.pathname.includes("/user/login")
+      ) {
+        window.location.href = `/user/login?redirect=${window.location.href}`;
+      }
+    }
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
+
+export default myAxios;
